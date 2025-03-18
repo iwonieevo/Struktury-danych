7fcb12c57cc4d2c713dbd0800079c0829b411418
Iwo Chwiszczuk <iwo.chwiszczuk@gmail.com>
2025-03-18 02:16:29 +0100

    Created function for timing purposes. Created a skelly of an idea how to perform the neccesary research

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 // Function loading data into List
 void load_from_file(const std::string& source_path, List& list) {
@@ -19,128 +20,33 @@ void load_from_file(const std::string& source_path, List& list) {
     file.close();
 }
 
-/* Function generating files with random integers. from - the smallest quantity of values, to - the largest quantity of values, step - step of quantity, times - number of copy for each step
- e.g. for from = 5000, to = 100000, step = 5000, times = 25, there will be 25 files for each of the quantities so 25 files of files containing 5k integers, 25 files containing 10k integers,..., 25 files containing 100k integers */
-void generate_data_files(size_t from, size_t to, size_t step, size_t times) {
-    for(size_t i = from; i <= to; i += step){
-        for(size_t j = 1; j <= times; j++){
-            std::string path = "data/random_data-" + std::to_string(i) + "-#" + std::to_string(j) + ".txt";
-            generate_random_integers(path, -10000, 10000, i);
-        }
-    }
+// Function that times method execution
+template <typename ReturnType, typename... Parameters>
+unsigned long long measure_time(List* list, ReturnType (List::*method)(Parameters...), Parameters... params) {
+    auto start = std::chrono::high_resolution_clock::now(); // start tracking time
+    (list->*method)(params...);
+    auto end = std::chrono::high_resolution_clock::now(); // stop tracking time
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    return duration.count();
 }
 
 int main(){
-    // generate_data_files(5000, 100000, 5000, 25);
-
     List* test;
-
-    std::cout << "Array List:\n";
-    test = new ArrayList;
-    test->append(4);
-    test->show(); // 4
-    test->prepend(5);
-    test->show(); // 5, 4
-    test->insert(3, 1);
-    test->show(); // 5, 3, 4
-    test->prepend(test->pop_back());
-    test->show(); // 4, 5, 3
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    test->remove(1);
-    test->show(); // 4, 3
-    test->pop_front();
-    test->show(); // 3
-    test->insert(10, 0);
-    test->show(); // 10, 3
-    test->insert(20, 2);
-    test->show(); // 10, 3, 20
-    test->remove(0);
-    test->show(); // 3, 20
-    test->append(300);
-    test->show(); // 3, 20, 300
-    test->remove(2);
-    test->show(); // 3, 20
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    delete test;
-
-    std::cout << "\nSingly Linked List:\n";
-    test = new SinglyLinkedList;
-    test->append(4);
-    test->show(); // 4
-    test->prepend(5);
-    test->show(); // 5, 4
-    test->insert(3, 1);
-    test->show(); // 5, 3, 4
-    test->prepend(test->pop_back());
-    test->show(); // 4, 5, 3
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    test->remove(1);
-    test->show(); // 4, 3
-    test->pop_front();
-    test->show(); // 3
-    test->insert(10, 0);
-    test->show(); // 10, 3
-    test->insert(20, 2);
-    test->show(); // 10, 3, 20
-    test->remove(0);
-    test->show(); // 3, 20
-    test->append(300);
-    test->show(); // 3, 20, 300
-    test->remove(2);
-    test->show(); // 3, 20
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    delete test;
-
-    std::cout << "\nDoubly Linked List:\n";
-    test = new DoublyLinkedList;
-    test->append(4);
-    test->show(); // 4
-    test->prepend(5);
-    test->show(); // 5, 4
-    test->insert(3, 1);
-    test->show(); // 5, 3, 4
-    test->prepend(test->pop_back());
-    test->show(); // 4, 5, 3
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    test->remove(1);
-    test->show(); // 4, 3
-    test->pop_front();
-    test->show(); // 3
-    test->insert(10, 0);
-    test->show(); // 10, 3
-    test->insert(20, 2);
-    test->show(); // 10, 3, 20
-    test->remove(0);
-    test->show(); // 3, 20
-    test->append(300);
-    test->show(); // 3, 20, 300
-    test->remove(2);
-    test->show(); // 3, 20
-    std::cout << "Searching '4' [1 - found, 0 - not found]: " << test->search(4) << std::endl;
-    delete test;
-
-    std::cout << "\nPress Enter to continue...";
-    std::cin.get();  // Waits for the user to press Enter
-    std::cout << "\n\nLoading data from files - ArrayList:\n";
-    test = new ArrayList(5000);
-    load_from_file("data/random_data-5000-#1.txt", *test);
-    test->show();
-    delete test;
-
-    std::cout << "\nPress Enter to continue...";
-    std::cin.get();  // Waits for the user to press Enter
-    std::cout << "\n\nLoading data from files - SinglyLinkedList:\n";
-    test = new SinglyLinkedList();
-    load_from_file("data/random_data-5000-#1.txt", *test);
-    test->show();
-    delete test;
-
-    std::cout << "\nPress Enter to continue...";
-    std::cin.get();  // Waits for the user to press Enter
-    std::cout << "\n\nLoading data from files - DoublyLinkedList:\n";
-    test = new DoublyLinkedList();
-    load_from_file("data/random_data-5000-#1.txt", *test);
-    test->show();
-    delete test;
+    size_t THE_LEAST = 5000;
+    size_t THE_MOST = 100000;
+    size_t STEP = 5000;
+    size_t NUM_OF_TIMES = 25;
+    for(size_t i = THE_LEAST; i <= THE_MOST; i += STEP) {
+        for(size_t j = 1; j <= NUM_OF_TIMES; j++) {
+            std::string path = "data/random_data-" + std::to_string(i) + "-#" + std::to_string(j) + ".txt";
+            // generate_random_integers(path, -10000, 10000, i);
+            test = new ArrayList(i);
+            load_from_file(path, *test);
+            auto time = measure_time(test, List::prepend, 10001);
+            std::cout << "ArrayList;" << i << ";#" << j << ";" << time << std::endl;
+            delete test;
+        }
+    }
+    // TODO: dokończyć dla pozostałych operacji i SD, przemyśleć jak zapisywać wyniki, przemyśleć formę badań
     return 0;
 }
